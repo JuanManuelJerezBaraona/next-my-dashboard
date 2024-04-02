8205641aa497aae50f3c57138f2b7d92966a7014
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-04-02 10:21:09 -0300

    Add favorite functionality to PokemonCard component

--- src/store/pokemons/pokemons.ts
+++ src/store/pokemons/pokemons.ts
@@ -14,12 +14,15 @@ interface PokemonsState {
 
 const initialState: PokemonsState = {
     '1': { id: '1', name: 'bulbasaur' },
+    '3': { id: '3', name: 'asdasdasdas' },
 }
 
 const pokemonsSlice = createSlice({
   name: 'pokemons',
   initialState,
-  reducers: {}
+  reducers: {
+    
+  }
 });
 
 export const {} = pokemonsSlice.actions
